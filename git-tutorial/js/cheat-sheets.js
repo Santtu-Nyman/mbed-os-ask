b5fbc56baca0af4e882dd661073c07c82595e519
Jarno Poikonen <reactionstack@gmail.com>
2018-12-12 23:40:07 +0200

    migrating the tutorial elsewhere

--- git-tutorial/js/cheat-sheets.js
+++
@@ -1,5 +0,0 @@
-var pdfs = document.getElementsByTagName("embed");
-for(var i = 0, j = pdfs.length; i < j; ++i){
-  pdfs[i].width = window.innerWidth * 0.8;
-  pdfs[i].height = window.innerWidth * 0.8;
-}
--- git-tutorial/js/iframe-expand.js
+++
@@ -1,5 +0,0 @@
-var iframes = document.getElementsByTagName("iframe");
-for(var i = 0, j = iframes.length; i < j; ++i){
-  iframes[i].width = window.innerWidth * 0.8;
-  iframes[i].height = window.innerWidth * 0.8;
-}
